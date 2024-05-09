80c7bcdb9bbb6e9fe9fdc1f4951423f93e929cfa
MisterFoxyDev <misterfoxydev@gmail.com>
2024-05-09 17:27:25 +0200

    feat(FicheLogement): :sparkles: utilisation de  useNavigate et useEffect pour rediriger vers /error lorsque idLogement inexistante

--- src/components/pages/fiche-logement/FicheLogement.jsx
+++ src/components/pages/fiche-logement/FicheLogement.jsx
@@ -1,14 +1,27 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import "./fiche-logement.scss";
 import DetailedCard from "../../organisms/detailedCard/DetailedCard";
 import { useEffect } from "react";
+import logements from "../../../assets/logements/logements.json";
 
 const FicheLogement = () => {
+  const { idLogement } = useParams();
+  const navigate = useNavigate();
+  const logement = logements.find((logement) => logement.id === idLogement);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  const { idLogement } = useParams();
+  useEffect(() => {
+    if (!logement) {
+      navigate("/error");
+    }
+  }, [logement, navigate]);
+
+  if (!logement) {
+    return null;
+  }
 
   return (
     <div>
